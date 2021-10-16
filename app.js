dac635d86f1482909af30c13e191cfdd65382df5
AkshayBenny <akshaybennygh@gmail.com>
2021-10-17 00:00:39 +0530

    completed

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 
 const express = require("express");
 const ejs = require("ejs");
+var _ = require("lodash");
 
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -51,6 +52,20 @@ app.post("/compose", (req, res) => {
   posts.push(post);
 
   res.redirect("/");
+});
+
+app.get("/posts/:post", (req, res) => {
+  const requestedRoute = _.kebabCase(_.lowerCase(req.params.post));
+
+  posts.forEach((post) => {
+    const title = _.kebabCase(_.lowerCase(post.title));
+    if (requestedRoute === title) {
+      res.render("post", {
+        title: post.title,
+        content: post.content,
+      });
+    }
+  });
 });
 
 app.listen(3000, function () {
